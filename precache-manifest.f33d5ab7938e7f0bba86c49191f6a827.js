043bb09a3e2a7c830477ceab03ead70e55166a7b
shaghayegh0 <shaghayegh.dehghanis@ryerson.ca>
2022-12-05 22:45:17 -0500

    Updates

---
+++ precache-manifest.f33d5ab7938e7f0bba86c49191f6a827.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbb4307a8d38e2b1f63816e0f74cabb",
+    "url": "/lab10/index.html"
+  },
+  {
+    "revision": "5ebc744a7a42e555260a",
+    "url": "/lab10/static/js/2.914d4cc4.chunk.js"
+  },
+  {
+    "revision": "39d970eae1208585c324",
+    "url": "/lab10/static/js/main.b6bb64ee.chunk.js"
+  },
+  {
+    "revision": "40bc10c1a032df835262",
+    "url": "/lab10/static/js/runtime-main.3cace01d.js"
+  }
+]);
